f9e905c15a640c12723c91f150851a365215576d
Lucca Miorelli <lucca.miorelli@gmail.com>
2024-07-02 18:40:05 -0300

    fet: add logs

--- app/processing/launches.py
+++ app/processing/launches.py
@@ -1,6 +1,7 @@
 from typing import List, Optional
 
 import pandas as pd
+from loguru import logger
 
 from app.models import Launch
 from app.services.aws import AWS
@@ -8,10 +9,17 @@ from app.services.db import Database
 from app.services.requester import SpaceXRequester
 
 
-# Get data
 def extract_launches() -> Optional[List[Launch]]:
+    """
+    Extracts launches from SpaceX API.
+
+    Returns:
+        List[Launch]: List of Launch objects.
+    """
     spacex_requester = SpaceXRequester()
     response = spacex_requester.request_launches()
+
+    logger.info(f"Extracted {len(response)} launches from `launches` endpoint.")
 
     try:
         launches: List[Launch] = [Launch(**launch) for launch in response]
@@ -20,9 +28,18 @@ def extract_launches() -> Optional[List[Launch]]:
         print(e)
 
 
-# Save raw data to S3 as JSON
 def save_raw_data(launches: List[Launch]) -> None:
+    """
+    Saves raw data to S3 as JSON.
+    
+    Args:
+        launches (List[Launch]): List of Launch objects.
+        
+    Returns:
+        None
+    """
     aws = AWS()
+    logger.info(f"Saving {len(launches)} launches to S3 as raw data.")
     aws.save_to_json(
         data=[launch.model_dump() for launch in launches],
         path="01_raw",
@@ -32,14 +49,32 @@ def save_raw_data(launches: List[Launch]) -> None:
 
 
 def type_casting(df: pd.DataFrame) -> pd.DataFrame:
-    # Force date_utc to datetime
+    """
+    Type casts DataFrame columns to appropriate types.
+    
+    Args:
+        df (pd.DataFrame): DataFrame to be type casted.
+        
+    Returns:
+        pd.DataFrame: Type casted DataFrame
+    """
+    # Force `date_utc` to datetime
     df["date_utc"] = pd.to_datetime(df["date_utc"])
     return df
 
 
-# Process data
 def process_launches(launches: List[Launch]) -> pd.DataFrame:
+    """
+    Processes the extracted launches data.
+    
+    Args:
+        launches (List[Launch]): List of Launch objects.
+        
+    Returns:
+        pd.DataFrame: Processed DataFrame
+    """
     df = pd.json_normalize([launch.model_dump() for launch in launches])
+    logger.info(f"Normalized JSON data. DataFrame shape: {df.shape}")
 
     # Explode 'cores' column
     df = df.explode("cores")
@@ -49,6 +84,8 @@ def process_launches(launches: List[Launch]) -> pd.DataFrame:
         cores_df.columns = [f"cores.{sub_col}" for sub_col in cores_df.columns]
         df = df.drop(columns=["cores"]).join(cores_df)
 
+    logger.info(f"Exploded 'cores' column. DataFrame shape: {df.shape}")
+
     # Explode 'failures' column
     df = df.explode("failures")
 
@@ -56,15 +93,28 @@ def process_launches(launches: List[Launch]) -> pd.DataFrame:
         failures_df = pd.json_normalize(df["failures"])
         failures_df.columns = [f"failures.{sub_col}" for sub_col in failures_df.columns]
         df = df.drop(columns=["failures"]).join(failures_df)
+    df = df.drop(columns=["fairings"])
+
+    logger.info(f"Exploded 'failures' column. DataFrame shape: {df.shape}")
 
     df = type_casting(df)
+    logger.info(f"Type casted DataFrame. DataFrame shape: {df.shape}")
 
-    return df.drop(columns=["fairings"])
+    return df
 
 
-# Save processed data to S3 as parquet
 def save_processed_data(launches_df: pd.DataFrame) -> None:
+    """
+    Saves processed data to S3 as parquet.
+    
+    Args:
+        launches_df (pd.DataFrame): Processed DataFrame.
+        
+    Returns:
+        None
+    """
     aws = AWS()
+    logger.info(f"Saving processed data to S3 as parquet.")
     aws.save_to_parquet(
         df=launches_df,
         path="02_processed",
@@ -73,9 +123,18 @@ def save_processed_data(launches_df: pd.DataFrame) -> None:
     return
 
 
-# Save processed data to database table
 def save_to_database(launches_df: pd.DataFrame) -> None:
+    """
+    Saves processed data to database table.
+    
+    Args:
+        launches_df (pd.DataFrame): Processed DataFrame.
+        
+    Returns:
+        None
+    """
     db = Database()
+    logger.info(f"Saving processed data to database.")
     db.load_dataframe(
         df=launches_df,
         table="launches",
@@ -85,6 +144,7 @@ def save_to_database(launches_df: pd.DataFrame) -> None:
 
 
 def main():
+    """Main function to orchestrate the data processing pipeline."""
     launches = extract_launches()
     save_raw_data(launches)
     processed_df = process_launches(launches)
